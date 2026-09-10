527a032b9218d080db594b2f0fb5abb104f86be4
SquallyCannon <malachi.arney@students.cvtech.edu>
2026-09-10 09:38:32 -0500

    Add files via upload

---
+++ Domain 4/Student/415-review.py
@@ -0,0 +1,14 @@
+'''This file is used for game were you do noting and lose. 
+Tells players what positions they have reached in each stage in a game.'''
+
+
+game_state = True
+game_lives = 1
+while game_lives <= 3:
+    for i in range(1,11):
+        print("You have reached position", i, "in game life", game_lives)
+    if game_state == True:
+        game_lives +=1
+print("Thank you for playing.")
+
+print(__doc__)
